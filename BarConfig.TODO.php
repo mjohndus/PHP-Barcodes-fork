40cba4ff1aa7dd53a2d7ceae4a5be2b307591ee8
Momchil Bozhinov <momchil@bojinov.info>
2019-10-10 11:58:46 +0300

    Create BarConfig.TODO.php
    PHP 7.4+ so not any time soon

---
+++ BarConfig.TODO.php
@@ -0,0 +1,84 @@
+<?php
+
+# PHP 7.4
+
+declare(strict_types=1);
+
+namespace configTest;
+
+require_once("examples\bootstrap.php");
+
+use Barcodes\BarColor;
+
+class CFG {
+
+	private $label;
+	private $colors;
+
+	function __construct()
+	{
+		$this->label = new class('label')
+		{
+			public int $w = 4;
+			public ?string $text = "BLABLA";
+		};
+
+		$this->colors = new class('colors')
+		{
+			public \Barcodes\BarColor $c0;
+			public \Barcodes\BarColor $c1;
+
+			function __construct()
+			{
+				$this->c0 = new \Barcodes\BarColor(0);
+				$this->c1 = new \Barcodes\BarColor(0);
+			}
+		};
+	}
+
+	private function mergeConfig($name, $myConfig)
+	{
+		try {
+			foreach($myConfig as $property => $value) {
+				$this->$name->$property = $value;
+			}
+		} catch (\TypeError $ex) {
+			$msg = $ex->getMessage();
+			$msg = str_replace("class@anonymous", $name, $msg);
+			die($msg);
+		}
+	}
+
+	public function add(string $branch, array $myConfig)
+	{
+		if (property_exists($this, $branch)){
+			$this->mergeConfig($branch, $myConfig);
+		} else {
+			die("CFG: No such branch - $branch");
+		}
+	}
+
+	public function get(string $branch, string $property)
+	{
+		if (isset($this->$branch)){
+			if (isset($this->$branch->$property)){
+				return $this->$branch->$property;
+			} else {
+				die("CFG: No such property -> $property for branch -> $branch");
+			}
+		} else {
+			die("CFG: No such branch -> $branch");
+		}
+	}
+}
+
+$cfg = new CFG();
+$cfg->add('label', ["w" => 6, "text" => NULL]);
+
+var_dump($cfg);
+
+$cfg->add('colors', ["c0" => new \Barcodes\BarColor(255)]);
+
+var_dump($cfg);
+
+var_dump($cfg->get('label', 'w'));
